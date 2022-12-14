4e2e3eb5187b77154f53633faf30797e1657fb70
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-12-14 16:11:09 +0100

    install sass, dipendenze. Create file vue, scss

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "AppHeader"
+}
+</script>
+
+<template>
+
+</template>
+<style lang="scss" scoped>
+@use './styles/variables.scss' as *;
+@use './styles/general.scss' as *;
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: "AppMain"
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+@use './styles/variables.scss' as *;
+@use './styles/general.scss' as *;
+</style>
